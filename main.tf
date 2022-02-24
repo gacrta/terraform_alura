0afdd300233ac8e6a8860e0d9ef2fd7a1640c412
Gabriel Almeida <gabrielcrta@gmail.com>
2022-02-24 20:15:31 -0300

    Create ssh security group

--- main.tf
+++ main.tf
@@ -23,3 +23,16 @@ resource "aws_instance" "dev" {
     }
 }
 
+resource "aws_security_group" "acesso-ssh" {
+  name = "acesso-ssh"
+  description = "acesso-ssh"
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["177.27.202.112/32"]
+  }
+  tags = {
+      Name = "ssh"
+  }
+}
